22b31075dfa76d0a56f6ca28b01578a4692099bf
Alessio Frittoli <35973186+alessiofrittoli@users.noreply.github.com>
2024-12-25 16:17:57 +0100

    tests: improved jest setup (#16)

--- __tests__/random.test.ts
+++ __tests__/random.test.ts
@@ -37,15 +37,25 @@ describe( 'randomNumUUID', () => {
 
 describe( 'randomUUID', () => {
 
+	afterAll( () => {
+		jest.clearAllMocks()
+	} )
+
 	if ( typeof crypto !== 'undefined' ) {
 		it( 'generates a properly formatted UUID with crypto.randomUUID', () => {
+			const cryptoRandomMock = jest.spyOn( crypto, 'randomUUID' )
 			const result = randomUUID()
 			expect( result.split( '-' ).length ).toBe( 5 )
+			expect ( cryptoRandomMock ).toHaveBeenCalledTimes( 1 )
 		} )
-		return
 	}
 
 	it( 'generates a properly formatted UUID string without crypto.randomUUID', () => {
+		if ( typeof crypto !== 'undefined' ) {
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			;( crypto as any ).randomUUID = undefined
+		}		
+
 		const result = randomUUID()
 		expect( result.split( '-' ).length ).toBe( 5 )
 	} )
