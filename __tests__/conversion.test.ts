0fa59977412b6e4bda76a4188a6c1e39c4c565de
Alessio Frittoli <35973186+alessiofrittoli@users.noreply.github.com>
2025-01-09 20:21:24 +0100

    feature/getMapRatio (#25)
    * feat: add `getMapRatio` utility function
    
    * tests: add `getMapRatio` unit tests

--- __tests__/conversion.test.ts
+++ __tests__/conversion.test.ts
@@ -1,4 +1,110 @@
-import { convertTo } from '@/conversion'
+import { convertTo, getMapRatio } from '@/conversion'
+
+
+describe( 'getMapRatio', () => {
+
+	const CacioEPepe = {
+		spaghettoni		: 320,
+		pecorino		: 200, // pecorino romano DOP is a must
+		pepe			: 5,
+		portions		: 4,
+	}
+	
+	
+	const Carbonara = {
+		spaghettoni	: 320,
+		guanciale	: 150,
+		tuorli		: 6,
+		pepe		: 5,
+		pecorino	: 50, // pecorino romano DOP is a must
+		portions	: 4,
+	}
+
+
+	it( 'computes ratios correctly based on the given key', () => {
+		expect( getMapRatio( CacioEPepe, 'spaghettoni' ) )
+			.toEqual(
+				{
+					spaghettoni		: 1,	// 1 to 1
+					pecorino		: 1.6,	// 1 to 1.6 ( 320 / 200 )
+					pepe			: 64,	// 1 to 64 ( 320 / 5 )
+					portions		: 80,	// 1 to 80 ( 320 / 4 )
+				}
+			)
+		
+		expect( getMapRatio( CacioEPepe, 'portions' ) )
+			.toEqual(
+				{
+					spaghettoni		: 0.0125,	// 1 to 0.0125 ( 4 / 320 )
+					pecorino		: 0.02,	// 1 to 0.02 ( 4 / 200 )
+					pepe			: 0.8,	// 1 to 0.8 ( 4 / 5 )
+					portions		: 1,	// 1 to 1
+				}
+			)
+	} )
+
+
+	it( 'returns raw values without rounding', () => {
+		const map = { a: 1.5, b: 3, c: 4.5 }
+		expect( getMapRatio( map, 'b' ) )
+			.toEqual( { a: 2, b: 1, c: 0.6666666666666666 } )
+	} )
+
+
+	it( 'handles zero values in numerator', () => {
+		const map = { a: 0, b: 20, c: 40 };
+		expect( getMapRatio( map, 'a' ) )
+			.toEqual( { a: 1, b: 0, c: 0 } )
+	} )
+	
+
+	it( 'handles zero values in denominator', () => {
+		const map = { a: 10, b: 0, c: 40 };
+		expect( getMapRatio( map, 'a' ) )
+			.toEqual( { a: 1, b: 0, c: 0.25 } )
+	} )
+
+
+	it( 'does not modify the original map', () => {
+		const map		= { a: 10, b: 20, c: 40 }
+		const original	= { ...map }
+	
+		getMapRatio( map, 'b' )
+	
+		expect( map ).toEqual( original )
+	} )
+
+
+	it( 'throws an Error if the key does not exist in the given map', () => {
+		// @ts-expect-error never use onions with Carbonara recipe.
+		expect( () => getMapRatio( Carbonara, 'cipolla' ) )
+			.toThrow(
+		  		'The provided `key` \'cipolla\' doesn\'t exist in the provided map.'
+			)
+	} )
+
+
+	it( 'throws a TypeError for invalid numerator', () => {
+		expect( () => getMapRatio( { a: NaN, b: 20 }, 'a' ) )
+			.toThrow(
+				`The provided 'a' value is not a number.`
+			)
+		
+		// @ts-expect-error negative testing
+		expect( () => getMapRatio( { a: 'string', b: 20 }, 'a' ) )
+			.toThrow(
+				`The provided 'a' value is not a number. string given.`
+			)
+		
+		// @ts-expect-error negative testing
+		expect( () => getMapRatio( { a: null, b: 20 }, 'a' ) )
+			.toThrow(
+				`The provided 'a' value is not a number. object given.`
+			)
+	} )
+
+} )
+
 
 describe( 'convertTo', () => {
 
--- src/conversion/index.ts
+++ src/conversion/index.ts
@@ -1,6 +1,98 @@
 import { getTypedMap } from '@alessiofrittoli/web-utils'
 
 /**
+ * Generates a new conversion map where each key's ratio is recalculated 
+ * relative to a specified reference key in the input object.
+ *
+ * @template T A key-value record object where the keys represent unique identifiers 
+ *             and the values are numerical values associated with each key.
+ * @template K The key of the reference item in the input object.
+ *
+ * @param map A key-value record object. The keys represent unique identifiers, 
+ *            and the values are numerical quantities associated with each key.
+ * @param key The reference key in the input object. All other keys' ratios 
+ *            will be recalculated relative to the value of this key.
+ *
+ * @returns A new record object where each key's value is the ratio of the reference key's value 
+ *          divided by the original value of the corresponding key.
+ *
+ * @throws A new Exception if the specified `key` does not exist in the provided `map`.
+ *
+ * @example
+ * 
+ * ```ts
+ * const Carbonara = {
+ * 	spaghettoni	: 320,
+ * 	guanciale	: 150,
+ * 	tuorli	: 6,
+ * 	pepe	: 5,
+ * 	pecorino	: 50,
+ * 	cipolla	: 0, // 0 or you'll be fired!
+ * 	portions	: 4,
+ * }
+ *
+ * // Generate a map with ratios relative to the 'portions' key
+ * console.log( getMapRatio( Carbonara, 'portions' ) )
+ * // Outputs: {
+ * //	spaghettoni	: 80,
+ * //	guanciale	: 37.5,
+ * //	tuorli		: 1.5,
+ * //	pepe		: 1.25,
+ * //	pecorino	: 12.5,
+ * //	cipolla		: 0,
+ * //	portions	: 1,
+ * // }
+ * 
+ * // Get Carbonara ready for 1 person
+ * console.log( convertTo( 1, getMapRatio( Carbonara, 'portions' ) ) )
+ * // Outputs:
+ * Map(6) {
+ * 	'spaghettoni' => { value: 80, ratio: 0.0125 },
+ * 	'guanciale' => { value: 37.5, ratio: 0.02666666666666667 },
+ * 	'tuorli' => { value: 1.5, ratio: 0.6666666666666666 },
+ * 	'pepe' => { value: 1.25, ratio: 0.8 },
+ * 	'pecorino' => { value: 12.5, ratio: 0.08 },
+ * 	'cipolla' => { value: 0, ratio: 0 },
+ * 	'portions' => { value: 1, ratio: 1 }
+ * }
+ * ```
+ */
+export const getMapRatio = <
+	T extends Record<string, number> = Record<string, number>,
+	K extends keyof T = keyof T
+>( map: T, key: K ) => {
+	if ( ! ( key in map ) ) {
+		throw new Error( `The provided \`key\` '${ String( key ) }' doesn't exist in the provided map.` )
+	}
+	
+	const _map		= { ...map }
+	const numerator	= map[ key ]
+
+	if ( numerator == null || isNaN( numerator ) ) {
+		let message = (
+			`The provided '${ String( key ) }' value is not a number.`
+		)
+		if ( typeof numerator !== 'bigint' && typeof numerator !== 'number' ) {
+			message += ` ${ typeof numerator } given.`
+		}
+		throw new TypeError( message )
+	}
+	
+	Object.keys( _map ).map( mapKey => {
+		const denominator	= _map[ mapKey ]!
+		let newValue		= numerator / denominator
+
+		if ( isNaN( newValue ) ) newValue = 1
+		if ( newValue === Infinity || newValue === -Infinity ) newValue = 0
+		
+		;( _map[ mapKey ] as number ) = newValue
+	} )
+
+	return _map
+}
+
+
+/**
  * A mapping of unit names to their corresponding conversion factors.
  * 
  * @example
@@ -21,21 +113,21 @@ export type ConversionMap = Record<string, number>
 export interface Conversion
 {
 	/** 
-	 * The converted value using the given `factor`.
+	 * The converted value using the given `ratio`.
 	 * 
 	 * Example:
-	 * If the input value is `100` and the factor is `1000`, the `value` will be `0.1`.
+	 * If the input value is `100` and the ratio is `1000`, the `value` will be `0.1`.
 	 */
 	value: number
 
 
 	/** 
-	 * The provided conversion factor used to calculate the `value`.
+	 * The provided conversion ratio used to calculate the `value`.
 	 * 
 	 * Example:
-	 * For kilometers in a `ConversionMap`, the factor might be `1000`.
+	 * For kilometers in a `ConversionMap`, the ratio might be `1000`.
 	 */
-	factor: number
+	ratio: number
 }
 
 
@@ -69,19 +161,25 @@ export type ConvertedUnit<T extends ConversionMap> = Record<keyof T, Conversion>
  *
  * const result = convertTo( 100, InMeters )
  * 
- * console.log( result.get( 'mile' ) )	// { value: 0.06213727366498068, measurement: 1609.34 }
- * console.log( result.get( 'yard') )	// { value: 109.36132983377078, measurement: 0.9144 }
- * console.log( result.get( 'foot' ) )	// { value: 328.0839895013123, measurement: 0.3048 }
+ * console.log( result.get( 'mile' ) )	// { value: 0.06213727366498068, ratio: 1609.34 }
+ * console.log( result.get( 'yard') )	// { value: 109.36132983377078, ratio: 0.9144 }
+ * console.log( result.get( 'foot' ) )	// { value: 328.0839895013123, ratio: 0.3048 }
  * ```
  */
 export const convertTo = <T extends ConversionMap>( input: number, map: T ) => (
 	getTypedMap<ConvertedUnit<T>>(
 		Object.keys( map ).map( unit => {
-			const factor = map[ unit ] as number
-			return [ unit, {
-				value	: input / factor,
-				factor	: factor,
-			} ]
+			const ratio	= map[ unit ] as number
+			let value	= input / ratio
+
+			if (
+				isNaN( value ) ||
+				value === Infinity ||
+				value === -Infinity
+			) {
+				value = 0
+			}
+			return [ unit, { value, ratio } ]
 		} )
 	)
 )
