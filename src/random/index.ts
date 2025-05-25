5bae5cbd2d0163789f9f2591d370a63a9e3afb48
Alessio Frittoli <35973186+alessiofrittoli@users.noreply.github.com>
2025-05-25 21:42:03 +0200

    feature/generateUUID (#48)
    * feat: add `generateUUID` function
    
    * tests: add `generateUUID` unit tests
    
    * docs: add `generateUUID` API Reference

--- src/random/index.ts
+++ src/random/index.ts
@@ -1,3 +1,6 @@
+export type UUID = `${ string }-${ string }-${ string }-${ string }-${ string }`
+
+
 /**
  * Generate a random number between the given range.
  * Equivalent JavaScript function of PHP `mt_rand` function.
@@ -23,6 +26,7 @@ export const mtRand = ( min: number | string = 0, max: number | string = 0x7FFFF
 
 /**
  * Generate random numeric UUID.
+ * 
  * Usefull when `crypto.randomUUID()` is not available.
  * 
  * The usage is discouraged since this function has been designed to be executed as fallback of {@link crypto.randomUUID()}. Use {@link randomUUID} instead.
@@ -36,18 +40,64 @@ export const randomNumUUID = () => (
 		mtRand( 0, 0x0fff ) | 0x4000, // 16 bits for 'time_hi_and_version', Four most significant bits holds version number 4
 		mtRand( 0, 0x3fff ) | 0x8000, // 16 bits, 8 bits for 'clk_seq_hi_res', 8 bits for 'clk_seq_low', Two most significant bits holds zero and one for variant DCE1.1
 		[ mtRand( 0, 0xffff ), mtRand( 0, 0xffff ), mtRand( 0, 0xffff ) ].join( '' ) // 48 bits for 'node'
-	].join( '-' )
+	].join( '-' ) as UUID
 )
 
 
 /**
+ * Generate random v4 UUID.
+ * 
+ * Usefull when `crypto.randomUUID()` is not available.
+ * 
+ * The usage is discouraged since this function has been designed to be executed as fallback of {@link crypto.randomUUID()}. Use {@link randomUUID} instead.
+ * 
+ * @returns A random v4 UUID.
+ */
+export const generateUUID = () => {
+
+	/** Current timestamp. */
+	let ts = new Date().getTime()
+
+	/** Time in microseconds since page-load or 0 if unsupported. */
+	let microts = performance.now() * 1000
+
+	return (
+		'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+			.replace( /[xy]/g, substring => {
+				let random = mtRand( 0, 16 )
+
+				if ( ts > 0 ) {
+					/**
+					 * Use timestamp until depleted.
+					 */
+					random	= ( ts + random ) % 16 | 0
+					ts		= Math.floor( ts / 16 )
+				} else {
+					/**
+					 * Use microseconds since page-load if supported.
+					 */
+					random	= ( microts + random ) % 16 | 0
+					microts	= Math.floor( microts / 16 )
+				}
+
+				return (
+					( substring === 'x' ? random : ( random & 0x3 ) | 0x8 )
+						.toString( 16 )
+				)
+			} )
+	) as UUID
+
+}
+
+
+/**
  * Generate random UUID.
- * Uses `crypto.randomUUID()` whenever possible. Fallback to {@link randomNumUUID}.
+ * Uses `crypto.randomUUID()` whenever possible. Fallback to {@link generateUUID}.
  * 
  * @returns Random UUID.
  */
-export const randomUUID = () => (
+export const randomUUID = (): UUID => (
 	typeof crypto !== 'undefined' && 'randomUUID' in crypto && typeof crypto.randomUUID === 'function'
 		? crypto.randomUUID()
-		: randomNumUUID()
+		: generateUUID()
 )
